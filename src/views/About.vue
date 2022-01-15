5947d8970d2dcb69feeaa2003d648c41fa434ff4
Markus Scheuring <mail@markusscheuring.de>
2022-01-15 01:36:05 +0100

    content About view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,48 @@
 <template>
-    <div class="about">
-        
+  <div class="about">
+    <div class="font-bold text-2xl mt-2 mb-3 underline">
+      Arbeitszeitenrechner
     </div>
+    <div class="text-justify">
+      In dieser Anwendung können Sie Ihre Arbeitszeiten berechnen. In der ersten
+      Spalte werden die Überstunden berechnet anhand dem vorher angegeben
+      Arbeitsende. Die zweite Spalte berechnet das Arbeitsende anhand der
+      gewünschte Über- / Minusstunden. Die berechneten Werte werden Grün
+      markiert. Ist die gesetzlich verpflichtende Pause länger als die
+      angegebene Pause wird auch diese in grün markiert.
+    </div>
+    <div class="text-justify mt-1">
+      Sollten Sie Fehler oder Funktionswünsche haben, wenden Sie sich an den
+      Entwickler über die im folgenden angegebene E-Mail Adresse.
+    </div>
+    <div class="text-xl mt-2 mb-1 underline">App</div>
+    <div>
+      Entwickler:
+      <a
+        class="underline"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://markusscheuring.de"
+        >Markus Scheuring</a
+      >
+    </div>
+    <div>
+      Kontakt:
+      <a
+        class="underline"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="mailto:apps@markusscheuring.de"
+        >apps@markusscheuring.de</a
+      >
+    </div>
+    <div>Version: 1.0</div>
+  </div>
 </template>
+
+<style scoped>
+.about {
+  margin-right: 10%;
+  margin-left: 10%;
+}
+</style>
